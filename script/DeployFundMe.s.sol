651cbda19852470732e38f435b0126d7b9bb9fdd
Alan <alanwang3250@gmail.com>
2023-07-09 15:17:41 +0800

    add mock contract

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -1,13 +1,18 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity ^0.8.18;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 import {FundMe} from "../src/FundMe.sol";
 
 contract DeployFundMe is Script {
-    function run() external {
+    function run() external returns (FundMe, HelperConfig) {
+        HelperConfig helperConfig = new HelperConfig(); // This comes with our mocks!
+        address priceFeed = helperConfig.activeNetworkConfig();
+
         vm.startBroadcast();
-        new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        FundMe fundMe = new FundMe(priceFeed);
         vm.stopBroadcast();
+        return (fundMe, helperConfig);
     }
 }
